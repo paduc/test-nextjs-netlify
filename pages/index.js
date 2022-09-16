b2f9369e9e36c8cdd0ee1420c5381aea03308262
Pierre Duchateau <pad@dabla.com>
2022-09-16 17:01:15 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Script from "next/script"
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
+
+export default class Home extends Component {
+  render() {
+    let { title, cats } = attributes;
+    return (
+      <>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </>
+    )
+  }
+}
