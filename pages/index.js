f8be28524ebf0c7398ab1159fc3dcd3958d81a7f
Pierre Duchateau <pad@dabla.com>
2022-09-19 17:39:17 +0200

    change title

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default class Home extends Component {
             <div className='section-container'>
               <div className='fr-grid-row fr-grid-row--gutters fr-py-6w'>
                 <div className='fr-col fr-col-12 fr-col-md-6'>
-                  <h1>La célérité c'est bien mais avec modération</h1>
+                  <h1>{title}</h1>
                 </div>
                 <div className='fr-col fr-col-12 fr-col-md-6'>
                   <div style={{ marginLeft: 80, position: 'relative' }}>
